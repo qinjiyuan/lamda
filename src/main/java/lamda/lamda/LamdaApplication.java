b5bcbeb5ae8c3f0e071ae3657d8267897319df3e
qinjiyuan <jiyuan.qin@foxmail.com>
2019-11-27 15:34:19 +0800

    Initial commit

---
+++ src/main/java/lamda/lamda/LamdaApplication.java
@@ -0,0 +1,13 @@
+package lamda.lamda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LamdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LamdaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lamda/lamda/LamdaApplicationTests.java
@@ -0,0 +1,13 @@
+package lamda.lamda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LamdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
